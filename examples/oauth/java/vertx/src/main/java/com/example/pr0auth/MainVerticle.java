60168d2e18a0890339d9677a3f804dd50afeaf11
twobiers <22715034+twobiers@users.noreply.github.com>
2022-07-21 19:18:40 +0200

    fix typo and make a sample request

--- examples/oauth/java/vertx/src/main/java/com/example/pr0auth/MainVerticle.java
+++ examples/oauth/java/vertx/src/main/java/com/example/pr0auth/MainVerticle.java
@@ -9,6 +9,8 @@ import io.vertx.ext.auth.oauth2.OAuth2Auth;
 import io.vertx.ext.auth.oauth2.OAuth2FlowType;
 import io.vertx.ext.auth.oauth2.OAuth2Options;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -18,22 +20,25 @@ public class MainVerticle extends AbstractVerticle {
   public void start(Promise<Void> startPromise) {
     var server = vertx.createHttpServer();
     var router = Router.router(vertx);
+    var client = WebClient.create(vertx, new WebClientOptions().setUserAgent("vertx-auth-oauth2"));
     var port = 8888;
 
+    // Normally you would extract this code into a OAuth2Auth provider class. See for example:
+    // https://github.com/vert-x3/vertx-auth/blob/master/vertx-auth-oauth2/src/main/java/io/vertx/ext/auth/oauth2/providers/GithubAuth.java
     var credentials = new OAuth2Options()
       .setFlow(OAuth2FlowType.AUTH_CODE)
-      .setClientId("<client-id>")
-      .setClientSecret("<client-secret>")
+      .setClientId("<client id>")
+      .setClientSecret("<client secret>")
       .setSite("https://pr0gramm.com")
       .setTokenPath("/api/oauth/createAccessToken")
       .setAuthorizationPath("/oauth/authorize")
       .setHeaders(new JsonObject()
-        .put("User-Agent", "vertx-auth-oauth2"));
+        .put("User-Agent", "vertx-auth-oauth2")); // The User-Agent is necessary to get pass CloudFlare
 
     var oauth2 = OAuth2Auth.create(vertx, credentials);
 
     var authorization_uri = oauth2.authorizeURL(new JsonObject()
-      .put("redirectUri", "http://localhost:" + port + "/callback")
+      .put("redirect_uri", "http://localhost:" + port + "/callback")
       .put("scope", "user.me")
       .put("state", "insert random state here"));
 
@@ -52,14 +57,29 @@ public class MainVerticle extends AbstractVerticle {
 
     router.route("/callback").handler(ctx -> {
       var authCode = ctx.request().getParam("code");
-      var token = new JsonObject()
+      // Note that the redirectUri parameter is not in snake_case
+      var tokenRequest = new JsonObject()
         .put("code", authCode)
-        .put("redirect_uri", "http://localhost:" + port + "/callback");
+        .put("redirectUri", "http://localhost:" + port + "/callback");
       var response = ctx.response();
-      oauth2.authenticate(token)
+
+      oauth2.authenticate(tokenRequest)
         .onSuccess(user -> {
-          LOGGER.info("User: " + user.principal().toString());
-          response.end();
+          LOGGER.info("User Principal: " + user.principal().toString());
+          String token = user.get("access_token");
+
+          client.getAbs("https://pr0gramm.com/api/user/me")
+            .putHeader("Authorization", "Bearer " + token)
+            .send()
+            .onSuccess(res -> {
+              var userInfo = res.bodyAsJsonObject();
+              LOGGER.info("User Info: " + userInfo.toString());
+              response.end(userInfo.toString());
+            })
+            .onFailure(err -> {
+              LOGGER.error("Error: " + err.getMessage());
+              response.end("Error: " + err.getMessage());
+            });
         })
         .onFailure(err -> {
           LOGGER.error("Access Token Error: " + err.getMessage(), err);
